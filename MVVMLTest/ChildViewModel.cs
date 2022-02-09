e14372a0b3e9cbc58c5596af78e93e0af9d3a57c
Artem Mordan <87715226+Xenos-134@users.noreply.github.com>
2022-02-09 17:40:55 +0000

    Add project files.

---
+++ MVVMLTest/ChildViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLTest
+{
+    public class ChildViewModel : ViewModelBase
+    {
+        public ChildViewModel()
+        {
+
+            Messenger.Default.Register<Amessage>(this, (action) => ReceiveAMessage(action));
+        }
+
+        private void ReceiveAMessage(Amessage msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}
---
+++ MVVMLTest/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLTest
+{
+    public class MainViewModel : ViewModelBase
+    {
+
+        public RelayCommand<string> triggerCommand { get; set; }
+        public MainViewModel()
+        {
+            triggerCommand = new RelayCommand<string>(Trigger, (string txt)=>true);
+        }
+
+
+
+        public void Trigger(string txt)
+        {
+            var msg = new Amessage() { TheText = "TEST TEXT" };
+            Messenger.Default.Send<Amessage>(msg);
+        }
+    }
+}
